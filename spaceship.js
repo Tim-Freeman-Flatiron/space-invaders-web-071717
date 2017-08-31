c44a02b3ca84747bf61116a61df34c16901a67b1
Tim Freeman <d.timothy.freeman@gmail.com>
2017-08-31 11:40:09 -0400

    done

---
+++ spaceship.js
@@ -0,0 +1,29 @@
+class Spaceship {
+	constructor(name, crew, phasers, shields) {
+		this.name = name
+		this.crew = crew
+		this.phasers = phasers
+		this.shields = shields
+		this.cloaked = false
+		this.warpDrive = "disengaged"
+		this.docked = this.isDocked() 
+		this.phasersCharge = "uncharged"
+		this.setCrew()
+	}
+
+	isDocked () {
+		if (this.crew.length === 0) {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	setCrew () {
+		for (let i = 0; i < this.crew.length; i++) {
+			this.crew[i].currentShip = this
+		}
+	}
+
+//end of class
+}
